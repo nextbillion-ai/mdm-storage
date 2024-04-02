846d75b126c1fae80ff991bb4915ceed06166519
Rui Xu <rui@nextbillion.ai>
2024-04-02 16:46:03 +0800

    adjust resource factor

--- pods.go
+++ pods.go
@@ -1,6 +1,7 @@
 package mdmstorage
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 	"time"
@@ -13,6 +14,7 @@ const (
 	PRunning
 	PFailed
 	PRemoved
+	PCached
 )
 
 // PodInfo table pods
@@ -27,13 +29,15 @@ type PodInfo struct {
 	Area            string    `gorm:"column:area"`
 	CurrentJobCount int       `gorm:"column:current_job_count"`
 	CurrentJob      string    `gorm:"column:current_job"`
-	ResourceMeta    string    `gorm:"column:resource_meta"`
+	ResourceMetaStr string    `gorm:"column:resource_meta"`
 	State           PodState  `gorm:"column:state"`
 	RetryTimes      uint8     `gorm:"column:retry_times"`
 	Meta            string    `gorm:"column:meta"`
 	CreateAt        time.Time `gorm:"column:created_at"`
 	UpdateAt        time.Time `gorm:"column:update_at"`
 	FinishedAt      time.Time `gorm:"column:finished_at"`
+
+	ResourceMeta *ResourceMeta `gorm:"-"`
 }
 
 func (p *PodInfo) Match(mode, area, option string) bool {
@@ -72,6 +76,39 @@ func (p *PodInfo) RemoveCurrentJob(taskID string, chunkID uint32) int {
 	}
 	p.CurrentJob = strings.Join(newCurrentJob, "|")
 	return removeCount
+}
+
+func (p *PodInfo) GetResourceMeta() *ResourceMeta {
+	if p.ResourceMetaStr == "" {
+		return nil
+	}
+	if p.ResourceMeta == nil {
+		res := new(ResourceMeta)
+		err := json.Unmarshal([]byte(p.ResourceMetaStr), res)
+		if err != nil {
+			return nil
+		}
+		p.ResourceMeta = res
+	}
+	return p.ResourceMeta
+}
+
+type ResourceMeta struct {
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+	Action    string `json:"action"`
+	Spec      struct {
+		Asset struct {
+			Type    string `json:"type"`
+			Release string `json:"release"`
+		} `json:"asset"`
+		App struct {
+			Replicas int    `json:"replicas"`
+			Parallel bool   `json:"parallel"`
+			Version  string `json:"version"`
+			Storage  string `json:"storage"`
+		} `json:"app"`
+	} `json:"spec"`
 }
 
 func (p *PodInfo) TableName() string {
