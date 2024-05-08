608253d167d33d22a3354ec765ccecc4ff491bcf
Rui Xu <rui@nextbillion.ai>
2024-05-08 14:01:55 +0800

    remove pod job status

--- pods.go
+++ pods.go
@@ -2,8 +2,6 @@ package mdmstorage
 
 import (
 	"encoding/json"
-	"fmt"
-	"strings"
 	"time"
 )
 
@@ -27,8 +25,6 @@ type PodInfo struct {
 	Option          string    `gorm:"column:option"`
 	Mode            string    `gorm:"column:mode"`
 	Area            string    `gorm:"column:area"`
-	CurrentJobCount int       `gorm:"column:current_job_count"`
-	CurrentJob      string    `gorm:"column:current_job"`
 	ResourceMetaStr string    `gorm:"column:resource_meta"`
 	State           PodState  `gorm:"column:state"`
 	RetryTimes      uint8     `gorm:"column:retry_times"`
@@ -37,45 +33,12 @@ type PodInfo struct {
 	UpdateAt        time.Time `gorm:"column:update_at"`
 	FinishedAt      time.Time `gorm:"column:finished_at"`
 
-	ResourceMeta *ResourceMeta `gorm:"-"`
+	ResourceMeta    *ResourceMeta `gorm:"-"`
+	CurrentJobCount int           `gorm:"-"`
 }
 
 func (p *PodInfo) Match(mode, area, option string) bool {
 	return p.Mode == mode && p.Area == area && p.Option == option
-}
-
-func (p *PodInfo) GetCurrentJob() []string {
-	if strings.Trim(p.CurrentJob, " ") == "" {
-		return make([]string, 0)
-	}
-	return strings.Split(p.CurrentJob, "|")
-}
-
-func (p *PodInfo) AppendCurrentJob(taskID string, chunkID uint32) {
-	job := fmt.Sprintf("%v::%v", taskID, chunkID)
-	if strings.Trim(p.CurrentJob, " ") == "" {
-		p.CurrentJob = job
-		return
-	}
-	currentJobs := strings.Split(p.CurrentJob, "|")
-	currentJobs = append(currentJobs, job)
-	p.CurrentJob = strings.Join(currentJobs, "|")
-}
-
-func (p *PodInfo) RemoveCurrentJob(taskID string, chunkID uint32) int {
-	job := fmt.Sprintf("%v::%v", taskID, chunkID)
-	currentJob := strings.Split(p.CurrentJob, "|")
-	removeCount := 0
-	newCurrentJob := make([]string, 0)
-	for _, v := range currentJob {
-		if v == job {
-			removeCount++
-			continue
-		}
-		newCurrentJob = append(newCurrentJob, v)
-	}
-	p.CurrentJob = strings.Join(newCurrentJob, "|")
-	return removeCount
 }
 
 func (p *PodInfo) GetResourceMeta() *ResourceMeta {
@@ -94,21 +57,18 @@ func (p *PodInfo) GetResourceMeta() *ResourceMeta {
 }
 
 type ResourceMeta struct {
-	Name      string `json:"name"`
-	Namespace string `json:"namespace"`
-	Action    string `json:"action"`
-	Spec      struct {
-		Asset struct {
-			Type    string `json:"type"`
-			Release string `json:"release"`
-		} `json:"asset"`
-		App struct {
-			Replicas int    `json:"replicas"`
-			Parallel bool   `json:"parallel"`
-			Version  string `json:"version"`
-			Storage  string `json:"storage"`
-		} `json:"app"`
-	} `json:"spec"`
+	Asset struct {
+		Type    string `json:"type"`
+		Release string `json:"release"`
+	} `json:"asset"`
+	App struct {
+		Area     string `json:"area"`
+		Mode     string `json:"mode"`
+		Replicas int    `json:"replicas"`
+		Parallel bool   `json:"parallel"`
+		Version  string `json:"version"`
+		Storage  string `json:"storage"`
+	} `json:"app"`
 }
 
 func (p *PodInfo) TableName() string {
